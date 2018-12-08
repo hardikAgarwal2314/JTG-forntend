bd9bbb4bae40269561be2fcd7e626b13d325a730
Hardik Agarwal <hardikagarwal@Hardiks-MacBook-Air.local>
2018-12-08 12:53:27 +0530

    test deploy 1

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express();
 app.use(bodyParser.json());
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
+app.get('/')
 var server = app.listen(process.env.PORT || 8080, function () {
   var port = server.address().port;
   console.log("App now running on port", port);
